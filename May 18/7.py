adc8c41b1433e47d9cc79323e17e26a9ec4d6eba
Bekarys <ChiriNNi@users.noreply.github.com>
2023-05-22 21:22:36 +0600

    Усовершенствование задачи

--- May 18/7.py
+++ May 18/7.py
@@ -1,5 +1,5 @@
-hours, minutes = map(int, input("Введите часы и минуты: ").split())
-day = hours * 60 + minutes 
-ost_day = 1440 - day 
-ost_hours = ost_day // 60
-print(f"Осталось {ost_hours} часов и {ost_day - (ost_hours * 60)}")
+hours, minutes = map(int, input("Введите текущее время (в формате ЧЧ:ММ): ").split(':'))
+current_minutes = hours * 60 + minutes
+minutes_per_day = 24 * 60
+time_left_minutes = minutes_per_day - current_minutes
+print(f"До следующего дня осталось {time_left_minutes // 60}:{time_left_minutes % 60}")
